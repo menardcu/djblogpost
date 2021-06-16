662fe537637de9f8d519a47d2f7095c1e03d15be
Menard Cu <menard.cu@webe.com.my>
2021-06-16 15:39:46 +0800

    Fix syntax error

--- django_project/urls.py
+++ django_project/urls.py
@@ -38,5 +38,5 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if setting.STATIC_ROOT:
+if settings.STATIC_ROOT:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
